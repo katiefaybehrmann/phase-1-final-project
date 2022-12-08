8e02189b0c801729bc67365095c765c9f17a5add
Katie <katiefaybehrmann@gmail.com>
2022-12-07 21:01:53 -0700

    Add render card function for clothing items

--- index.js
+++ index.js
@@ -4,4 +4,16 @@ function getClothes(genre){
       .then((clothingData) => clothingData.forEach(item => renderClothes(item)))
 }
 
-function renderClothes(){}
+function renderClothes(item){
+    let card = document.createElement('li')
+    card.className = "card"
+    card.innerHTML = `
+      <h2>"${item.company}"</h2>
+      <h2>"${item.price}"</h2>
+      <img src="${item.image}" class="image-avatar">
+      <p>Labor Score: ${item.laborScore}</p>
+      <p>Environment Score: ${item.environmentScore}</p>
+      <button class="like-btn" id="${item.id}">Like ❤️</button>
+      `
+    document.querySelector("#collections").appendChild(card)
+}
